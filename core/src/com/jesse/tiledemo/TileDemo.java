bbf33b291ca7c48e1a604ccc913ab82f4d261a61
Jesse Stein <grand_beach_19@hotmail.com>
2015-09-04 03:17:44 -0500

    added camera, modified tile picking

--- core/src/com/jesse/tiledemo/TileDemo.java
+++ core/src/com/jesse/tiledemo/TileDemo.java
@@ -2,8 +2,10 @@ package com.jesse.tiledemo;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
 import com.jesse.tiledemo.mapsystem.Map;
@@ -14,34 +16,59 @@ public class TileDemo extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Map map;
 	private MapRenderer renderer;
-	
-	
+	private OrthographicCamera camera;
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
-		try{
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+		camera = new OrthographicCamera(w,h);
+		camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+		camera.update();
+
+		try {
 			map = MapLoader.loadMap("map1.txt");
 			renderer = new MapRenderer(batch, map);
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		Gdx.input.setInputProcessor(new InputAdapter(){
-			public boolean touchDown(int screenX, int screenY, int pointer, int button){
+
+		Gdx.input.setInputProcessor(new InputAdapter() {
+			public boolean touchDown(int screenX, int screenY, int pointer, int button) {
 				Vector2 mouse = new Vector2(Gdx.input.getX(), Gdx.input.getY());
-				map.getTileAt(mouse);
+				map.getTileAt(mouse,camera);
 				return true;
 			}
 		});
-		
+
 	}
 
 	@Override
-	public void render () {
+	public void render() {
+		handleInput();
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
+		
 		Gdx.gl.glClearColor(0, 0, 0, 0);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 			renderer.render();
 		batch.end();
+	}
+
+	private void handleInput() {
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			camera.translate(-3, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			camera.translate(3, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			camera.translate(0, -3, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			camera.translate(0, 3, 0);
+		}
 	}
 }
--- core/src/com/jesse/tiledemo/mapsystem/Map.java
+++ core/src/com/jesse/tiledemo/mapsystem/Map.java
@@ -25,15 +25,25 @@ public class Map {
         tiles = new ArrayList<Tile>();
     }
     
-    public void getTileAt(Vector2 mouse){
+    public void getTileAt(Vector2 mouse, OrthographicCamera camera){
+    	//Inverse y-axis to correct for input origin
     	mouse.y = Gdx.graphics.getHeight()-mouse.y;
+    	
+    	//Get camera offset
+    	int camOffsetX = (int) (camera.position.x - camera.viewportWidth/2); 
+    	int camOffsetY = (int) (camera.position.y - camera.viewportHeight/2); 
+    	
+    	mouse.x += camOffsetX;
+    	mouse.y += camOffsetY;
+    	
+    	
+    	
     	int tileX = (int) (mouse.x/TILE_SIZE);
     	int tileY = (int) (mouse.y/TILE_SIZE);
     	System.out.println("("+tileX+" , "+tileY+")");
     }
     
-    
-    
+
     
     
     public int getTilesX(){
